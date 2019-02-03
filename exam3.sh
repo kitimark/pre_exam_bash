2e3f3a2a3f9a4c1ff07185d7310e291397373a0b
mrkwskiti <markdog_@hotmail.com>
2019-02-03 22:30:48 +0700

    Uncomment exam3

--- exam3.sh
+++ exam3.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-## Change name files
-#for i in `find data?.txt`;do
-#	j=`echo $i | sed -e "s/data//g" | sed -e "s/.txt//g"`
-#	mv $i data0$j.txt
-#done
-#
-## Move files
-#for i in `find data*`;do
-#	j=`echo $i | sed -e "s/data//g" | sed -e "s/.txt//g"`
-#	mkdir Data$j
-#	mv $i Data$j/
-#done
+# Change name files
+for i in `find data?.txt`;do
+	j=`echo $i | sed -e "s/data//g" | sed -e "s/.txt//g"`
+	mv $i data0$j.txt
+done
+
+# Move files
+for i in `find data*`;do
+	j=`echo $i | sed -e "s/data//g" | sed -e "s/.txt//g"`
+	mkdir Data$j
+	mv $i Data$j/
+done
 
 mkdir myBackupProgram
 for i in `find mydata*`;do
